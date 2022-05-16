db2a8fa0f7c36cba595d3ca87b3267c3cdb74ca2
Fernando <fernandounlu@hotmail.com>
2022-05-16 13:01:58 -0300

    02

--- src/main/java/com/portfolio/Fernando/FernandoApplication.java
+++ src/main/java/com/portfolio/Fernando/FernandoApplication.java
@@ -2,8 +2,7 @@ package com.portfolio.Fernando;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class FernandoApplication {
@@ -11,14 +10,8 @@ public class FernandoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FernandoApplication.class, args);
 	}
-//pegar acá
-     	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
-			}
-		};
-	}   
+
+
+	   
         
 }
